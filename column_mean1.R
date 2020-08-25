c03eb6490e23a0fa4f3297dc7325805c9bee8b31
MonkeY <17602224@velsuniv.ac.in>
2020-08-25 23:06:59 +0530

    changed code to remova na

--- column_mean1.R
+++ column_mean1.R
@@ -1,8 +1,8 @@
-column_mean1<-function(a){
+column_mean1<-function(a,removeNA=TRUE){
   nc<-ncol(a)
   vector_means<-numeric(nc)
   for(i in 1:nc){
-    vector_means[i]<-mean(a[,i],rm.na=TRUE)
+    vector_means[i]<-mean(a[,i],na.rm=removeNA)
   }
   vector_means
 }
